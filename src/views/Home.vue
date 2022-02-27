41b0f767274847398fc5c35fc9cb10b40e73f4ed
Olzhas <oljasalidarov@gmail.com>
2022-02-27 19:22:56 +0600

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="home">
+
+    </div>
+    <!-- <calculator /> -->
+  </div>
+</template>
+
+<script>
+// import calculator from '@/components/calculator.vue'
+export default {
+  name: "Home",
+  components: {
+    // calculator
+  }
+};
+</script>
