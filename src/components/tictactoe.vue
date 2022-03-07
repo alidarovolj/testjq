39feec911210a501beb4d30d2a6fc486e0d8efeb
Olzhas <oljasalidarov@gmail.com>
2022-03-07 20:15:05 +0600

    tictac

--- src/components/tictactoe.vue
+++ src/components/tictactoe.vue
@@ -1,45 +1,111 @@
 <template>
-    <div class="container">
-        <h1 class="text-3xl my-3 text-center font-bold">TicTacToe</h1>
-        <div class="flex flex-wrap w-2/3 mx-auto">
-            <div class="slot py-4 font-bold text-center w-1/3 text-3xl">
-                &nbsp;
-            </div>
-            <div class="slot py-4 font-bold text-center w-1/3 text-3xl border-r-4 border-l-4 border-black">
-                &nbsp;
-            </div>
-            <div class="slot py-4 font-bold text-center w-1/3 text-3xl">
-                &nbsp;
-            </div>
+  <div class="container">
+    <h1 class="text-3xl my-3 text-center font-bold">TicTacToe</h1>
+    <p class="turn"></p>
+    <div class="flex flex-wrap w-2/3 mx-auto">
+      <div class="slot py-4 font-bold text-center w-1/3 text-3xl">&nbsp;</div>
+      <div
+        class="
+          slot
+          py-4
+          font-bold
+          text-center
+          w-1/3
+          text-3xl
+          border-r-4 border-l-4 border-black
+        "
+      >
+        &nbsp;
+      </div>
+      <div class="slot py-4 font-bold text-center w-1/3 text-3xl">&nbsp;</div>
 
-            <div class="slot py-4 font-bold text-center w-1/3 text-3xl border-t-4 border-b-4 border-black">
-                &nbsp;
-            </div>
-            <div class="slot py-4 font-bold text-center w-1/3 text-3xl border-4 border-black">
-                &nbsp;
-            </div>
-            <div class="slot py-4 font-bold text-center w-1/3 text-3xl border-t-4 border-b-4 border-black">
-                &nbsp;
-            </div>
+      <div
+        class="
+          slot
+          py-4
+          font-bold
+          text-center
+          w-1/3
+          text-3xl
+          border-t-4 border-b-4 border-black
+        "
+      >
+        &nbsp;
+      </div>
+      <div
+        class="
+          slot
+          py-4
+          font-bold
+          text-center
+          w-1/3
+          text-3xl
+          border-4 border-black
+        "
+      >
+        &nbsp;
+      </div>
+      <div
+        class="
+          slot
+          py-4
+          font-bold
+          text-center
+          w-1/3
+          text-3xl
+          border-t-4 border-b-4 border-black
+        "
+      >
+        &nbsp;
+      </div>
 
-            <div class="slot py-4 font-bold text-center w-1/3 text-3xl">
-                &nbsp;
-            </div>
-            <div class="slot py-4 font-bold text-center w-1/3 text-3xl border-r-4 border-l-4 border-black">
-                &nbsp;
-            </div>
-            <div class="slot py-4 font-bold text-center w-1/3 text-3xl">
-                &nbsp;
-            </div>
-        </div>
+      <div class="slot py-4 font-bold text-center w-1/3 text-3xl">&nbsp;</div>
+      <div
+        class="
+          slot
+          py-4
+          font-bold
+          text-center
+          w-1/3
+          text-3xl
+          border-r-4 border-l-4 border-black
+        "
+      >
+        &nbsp;
+      </div>
+      <div class="slot py-4 font-bold text-center w-1/3 text-3xl">&nbsp;</div>
     </div>
+  </div>
 </template>
 
 <script>
+import $ from "jquery";
+
 export default {
-    name: "TicTacToe",
-    mounted() {
-        console.log('hjdksa');
+  name: "TicTacToe",
+  mounted() {
+    var x = "X";
+    var o = "O";
+    var turn = Math.ceil(Math.random() * 2);
+
+    if (turn == 1) {
+      $(".turn").html("Ход: " + x);
+    } else {
+      $(".turn").html("Ход: " + o);
     }
-}
+
+    $(".slot").on("click", function () {
+      if (turn == 1) {
+        $(this).html(x);
+        turn = 2;
+        $(".turn").html("Ход: " + o);
+      } else {
+        $(this).html(o);
+        turn = 1;
+        console.log("Ходит крестик");
+        $(".turn").html("Ход: " + x);
+      }
+    });
+  },
+};
 </script>
