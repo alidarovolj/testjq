e6bb6e002e4357e4538d1f332970bba1abaf7389
Olzhas <oljasalidarov@gmail.com>
2022-03-07 19:52:07 +0600

    hooks

--- src/components/tictactoe.vue
+++ src/components/tictactoe.vue
@@ -1,6 +1,45 @@
 <template>
-    <div class="flex">
-        <p>dsadsa</p>
-        <p>dsadsa</p>
+    <div class="container">
+        <h1 class="text-3xl my-3 text-center font-bold">TicTacToe</h1>
+        <div class="flex flex-wrap w-2/3 mx-auto">
+            <div class="slot py-4 font-bold text-center w-1/3 text-3xl">
+                &nbsp;
+            </div>
+            <div class="slot py-4 font-bold text-center w-1/3 text-3xl border-r-4 border-l-4 border-black">
+                &nbsp;
+            </div>
+            <div class="slot py-4 font-bold text-center w-1/3 text-3xl">
+                &nbsp;
+            </div>
+
+            <div class="slot py-4 font-bold text-center w-1/3 text-3xl border-t-4 border-b-4 border-black">
+                &nbsp;
+            </div>
+            <div class="slot py-4 font-bold text-center w-1/3 text-3xl border-4 border-black">
+                &nbsp;
+            </div>
+            <div class="slot py-4 font-bold text-center w-1/3 text-3xl border-t-4 border-b-4 border-black">
+                &nbsp;
+            </div>
+
+            <div class="slot py-4 font-bold text-center w-1/3 text-3xl">
+                &nbsp;
+            </div>
+            <div class="slot py-4 font-bold text-center w-1/3 text-3xl border-r-4 border-l-4 border-black">
+                &nbsp;
+            </div>
+            <div class="slot py-4 font-bold text-center w-1/3 text-3xl">
+                &nbsp;
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    name: "TicTacToe",
+    mounted() {
+        console.log('hjdksa');
+    }
+}
+</script>
