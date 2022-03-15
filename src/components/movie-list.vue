60f29939f0067261902d8e5218aeb8ec595ce6ec
Olzhas <oljasalidarov@gmail.com>
2022-03-15 21:47:03 +0600

    POST and GET

--- src/components/movie-list.vue
+++ src/components/movie-list.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="container mx-auto">
-    <div class="border-b py-8 border-gray-300" v-for="movie of movies" :key="movie.id">
-      <div class="flex items-start">
+    <div
+      class="border-b py-8 border-gray-300"
+      v-for="movie of movies"
+      :key="movie.id"
+    >
+      <div
+        @click="$router.push({ name: 'Movie', params: { id: movie.id } })"
+        class="flex items-start"
+      >
         <div class="mr-4 relative">
-            <img :src="movie.img" alt="" />
-            <img class="mustsee_movie" v-if="movie.mustsee === true" src="https://www.metacritic.com/images/icons/mc-mustsee-sm.svg" alt="">
+          <img :src="movie.img" alt="" />
+          <img
+            class="mustsee_movie"
+            v-if="movie.mustsee === true"
+            src="https://www.metacritic.com/images/icons/mc-mustsee-sm.svg"
+            alt=""
+          />
         </div>
         <div class="block w-full">
           <h2 class="text-xl font-semibold">
@@ -18,26 +30,123 @@
         </p>
       </div>
     </div>
+    <form class="my-20" action="">
+      <h1 class="text-3xl font-bold my-4">Enter new data</h1>
+      <p class="font-bold my-2">Enter title...</p>
+      <input
+        class="border border-gray-300 text-lg w-full p-3 rounded-lg"
+        type="text"
+        v-model="form.title"
+      />
+      <p class="font-bold my-2">Enter date...</p>
+      <input
+        class="border border-gray-300 text-lg w-full p-3 rounded-lg"
+        type="text"
+        v-model="form.date"
+      />
+      <p class="font-bold my-2">Enter description...</p>
+      <input
+        class="border border-gray-300 text-lg w-full p-3 rounded-lg"
+        type="text"
+        v-model="form.desc"
+      />
+      <p class="font-bold my-2">Enter metascore...</p>
+      <input
+        class="border border-gray-300 text-lg w-full p-3 rounded-lg"
+        type="text"
+        v-model="form.rate"
+      />
+      <p class="font-bold my-2">Enter userscore...</p>
+      <input
+        class="border border-gray-300 text-lg w-full p-3 rounded-lg"
+        type="text"
+        v-model="form.user"
+      />
+      <p class="font-bold my-2">Enter img url...</p>
+      <input
+        class="border border-gray-300 text-lg w-full p-3 rounded-lg"
+        type="text"
+        v-model="form.img"
+      />
+      <p class="font-bold my-2">Enter video url...</p>
+      <input
+        class="border border-gray-300 text-lg w-full p-3 rounded-lg"
+        type="text"
+        v-model="form.video"
+      />
+      <p class="font-bold my-2">Enter mustsee...</p>
+      <input
+        class="border border-gray-300 text-lg w-full p-3 rounded-lg"
+        type="text"
+        v-model="form.mustsee"
+      />
+      <button
+        class="
+          bg-green-400
+          text-white
+          px-20
+          py-3
+          text-2xl
+          flex
+          mx-auto
+          rounded-lg
+          mt-4
+        "
+        @click="addMovie()"
+      >
+        Commit movie
+      </button>
+    </form>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+
 export default {
   data() {
     return {
       movies: [],
-      baseURL: "",
+      baseURL: "http://localhost:3001/movies",
+      form: {
+        title: "",
+        date: "",
+        desc: "",
+        rate: 0,
+        user: 0,
+        img: "",
+        mustsee: false,
+        video: "",
+      },
     };
   },
   async mounted() {
     try {
-      const res = await axios.get(`http://localhost:3001/movies`);
+      const res = await axios.get(this.baseURL);
       this.movies = res.data;
-      console.log(res);
     } catch (e) {
       console.error(e);
     }
+    console.log(this.movies);
+  },
+  methods: {
+    async addMovie() {
+      try {
+        const res = await axios.post(this.baseURL, {
+          title: this.form.title,
+          date: this.form.date,
+          desc: this.form.desc,
+          rate: this.form.rate,
+          user: this.form.user,
+          img: this.form.img,
+          mustsee: this.form.mustsee,
+          video: this.form.video,
+        });
+        this.movies = [...this.movies, res.data];
+      } catch (e) {
+        console.error(e);
+      }
+    },
   },
 };
 </script>
